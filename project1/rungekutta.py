52e160447fb089b9b6b85999ebd68220faab76e7
Elias <elias.bjork@icloud.com>
2022-11-09 17:10:20 +0100

    RK34step implemented

--- project1/rungekutta.py
+++ project1/rungekutta.py
@@ -19,6 +19,27 @@ def RK4step(f: Callable[[float, npt.NDArray], npt.NDArray], t_old: float,
     return u_old + h / 6 * (dY1 + 2 * dY2 + 2 * dY3 + dY4)
 
 
+def RK34step(f: Callable[[float, npt.NDArray], npt.NDArray], t_old: float,
+            u_old: npt.NDArray, h: float) -> Tuple[npt.NDArray, npt.NDArray]:
+
+    """Performs one step for the equation y'=f(t, y) using the RK4 schema and
+    estimates the local error using the embedded RK3.
+    
+    Returns: the next step, and the local error estimate for the next step
+    """
+    
+    dY1 = f(t_old, u_old)
+    dY2 = f(t_old + h/2, u_old + h * dY1 / 2)
+    dY3 = f(t_old + h/2, u_old + h * dY2 / 2)
+    dZ3 = f(t_old + h, u_old - h * dY1 + 2 * h * dY2)
+    dY4 = f(t_old + h, u_old + h * dY3)
+
+    u_new = u_old + h / 6 * (dY1 + 2 * dY2 + 2 * dY3 + dY4)
+    local_err_new = h / 6 * (2 * dY2 + dZ3 - 2 * dY3 - dY4)
+
+    return u_new, local_err_new
+
+
 def RK4_test_solver(f: Callable[[float, npt.NDArray], npt.NDArray],
                     true_solution:  Callable[[float], npt.NDArray],
                     u0: npt.NDArray,

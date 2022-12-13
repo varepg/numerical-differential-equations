6d28ca5338c209d137fedcbd633563b3ac778b42
emmaramne <eramne99@gmail.com>
2022-12-13 15:50:25 +0100

    added plots for laxwen

--- project3/plotting.py
+++ project3/plotting.py
@@ -2,6 +2,7 @@ import numpy as np
 import matplotlib.pyplot as plt
 from mpl_toolkits.mplot3d import axes3d
 from diffusion import solve_diffusion
+from laxwen import get_nbr_timesteps, get_stepsizes, laxwen_stepper
 
 
 def task12():
@@ -18,5 +19,29 @@ def task12():
     plt.show()
 
 
+def task21():
+    g = lambda x: np.exp(-100*(x-0.5)**2)
+    N = 100
+    xgrid = np.linspace(0,1,N)
+    amu = 1
+    a = 0.5
+    dt, dx = get_stepsizes(a, amu, N)
+    dt = amu * dx / a
+    u0 = g(xgrid)
+    M = get_nbr_timesteps(5, dt)
+    tgrid = np.linspace(0, 5, M)
+    u, norms = laxwen_stepper(u0, amu, M)
+    T, X = np.meshgrid(tgrid, xgrid)
+    fig = plt.figure()
+    ax = fig.add_subplot(projection='3d')
+    ax.plot_surface(T, X, u, rstride=1, cstride=1, cmap="viridis", edgecolor="none")
+    ax.set_xlabel("t")
+    ax.set_ylabel("x")
+    plt.show()
+    plt.figure(2)
+    plt.plot(tgrid, norms)
+    plt.show()
+
+
 if __name__ == "__main__":
-    task12()
+    task21()

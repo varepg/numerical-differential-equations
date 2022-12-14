849134afb280eb75d51e3565e52a5b39d352c3c5
Elias <elias.bjork@icloud.com>
2022-12-14 14:35:09 +0100

    added documentation

--- project3/convdiv.py
+++ project3/convdiv.py
@@ -1,10 +1,19 @@
 import numpy as np
 import scipy.sparse as sparse
 
+from typing import Tuple
 from numpy.typing import NDArray
 
 
 def create_circulant_Tdx(dx: float) -> sparse.csr_array:
+    """Creates a sparse matrix representation of the second derivative operator
+    with periodic boundary conditions.
+
+    Parameters:
+    - dx: stepsize of spatial grid.
+
+    Returns: the sparse matrix in CSR format.
+    """
     N = int(1 / dx)
     diagonals = [[-2]*N, [1]*(N-1), [1]*(N-1), [1], [1]]
     Tdx = 1/(dx**2) * sparse.diags(diagonals, [0, -1, 1, -N + 1, N - 1])
@@ -13,6 +22,14 @@ def create_circulant_Tdx(dx: float) -> sparse.csr_array:
 
 
 def create_circulant_Sdx(dx: float) -> sparse.csr_array:
+    """Creates a sparse matrix representation of the first derivative operator
+    with periodic boundary conditions.
+
+    Parameters:
+    - dx: stepsize of spatial grid.
+
+    Returns: the sparse matrix in CSR format.
+    """
     N = int(1 / dx)
     diagonals = [[-1]*(N-1), [1]*(N-1), [1], [-1]]
     Sdx = 1/(2*dx) * sparse.diags(diagonals, [ -1, 1, -N + 1, N - 1])
@@ -20,14 +37,24 @@ def create_circulant_Sdx(dx: float) -> sparse.csr_array:
     return Sdx
 
 
-def convdiv(u: NDArray, a: float, d: float, dt: float, dx: float):
+def convdiv(u: NDArray, a: float, d: float, dt: float, dx: float) -> NDArray:
+    """Performs one timestep in the convection-diffusion equation.
+
+    Parameters:
+    - u: the previous timestep over a spatial grid.
+    - a: constant of convection.
+    - d: constant of diffusivity.
+    - dt: size of timestep.
+    - dx: size of spatial step.
+
+    Returns: the next step.
+    """
     Tdx = create_circulant_Tdx(dx)
     Sdx = create_circulant_Sdx(dx)
 
     A = d*Tdx - a*Sdx
 
     N = int(1/dx)
-
 
     B = (sparse.eye(N, format="csr") - dt/2*A)
     C = (sparse.eye(N, format="csr") + dt/2*A)
@@ -40,7 +67,17 @@ def convdiv_stepper(
         a: float,
         d: float,
         M: int
-    ) -> NDArray:
+    ) -> Tuple[NDArray, NDArray]:
+    """Solves the convection-diffusion equation with initial condition u0.
+
+    Parameters:
+    - u0: initial condition.
+    - a: constant of convection.
+    - d: constant of diffusivity.
+    - M: number of timesteps.
+
+    Returns: the solution matrix u and the timegrid tgrid.
+    """
 
     u = np.reshape(u0, (np.size(u0),1))
     dt = 1/M
